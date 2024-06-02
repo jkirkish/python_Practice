25762fdff91fbf64417346bfd1a2176aee741641
Joseph Kirkish <kirkish.2@gmail.com>
2024-06-01 22:45:56 -0400

    added more to app2.py

--- app2.py
+++ app2.py
@@ -36,3 +36,49 @@ file.write(message)
 # None is the abscence of a value
 # unless you make the function return something
 print(greet1("Mosh", "Josh"))
+
+
+def increment(number, by):
+    return number + by
+
+
+# here shows a keyword argument to make the parameters more clearer increment2 by 1
+print(increment(2, by=1))
+
+# default arguments
+
+
+def increment1(number, by=1):
+    return number + by
+
+
+print(increment1(2))
+
+
+def multiply(*numbers):
+    total = 1
+    for number in numbers:
+        total *= number
+    return total
+
+
+print(multiply(2, 3, 4, 5))
+
+
+def save_user(**user):
+    print(user)
+    print(user[id])
+    print(user["name"])
+    print(user[age])
+
+
+save_user(id=1, name="John", age=22)
+# {'id': 1, 'name': 'John', 'age': 22} this is a dicitionary
+
+# (2,3,4,5) is a tuple
+# [2,3,4,5] is a list
+
+# best practice is create functions with local variables.  Global variables only use them
+# if you have too.
+# F9 is used to add a breakpoint
+# f5 opens the integreated terminal
