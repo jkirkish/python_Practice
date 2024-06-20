dcfaebea59a8f02d32fb4ba3d32b49dbf5e8db6c
Joseph Kirkish <kirkish.2@gmail.com>
2024-06-19 23:30:27 -0400

    more about stacks and lists

--- app3.py
+++ app3.py
@@ -39,3 +39,72 @@ letters = ["a","b","c"]
 items = [0,"a"]
 for index, letter in enumerate(letters):
     print(index, letter)
+
+#add something to the end of the list
+letters.append("d")
+#add something in a specifcic index 
+letters.insert(0, "-")
+print(letters)
+
+#remove something at the end of the list
+letters.pop()
+#remove the first index value of the list
+letters.pop(0)
+#remove the first occurrence of b
+letters.remove("b")
+del letters[0:3] # range of items
+letters. clear() # removes everything in the list
+print(letters)
+
+letters = ["a","b","c"]
+print(letters.count("d"))
+if "d" in letters:
+    print(letters.index("d")) # trying to get the index that doesn't exist, you get an error
+
+numbers = [3,51,2,8,6]
+# numbers.sort(reverse=True)
+print(sorted(numbers, reverse=True))
+print(numbers)
+
+items = [
+    ("Product1",10),
+    ("Product2", 9),
+    ("Product3",12),
+]
+
+#def sort_item(item):
+    #return item[1]
+
+#items.sort(key=sort_item)
+# prices = []
+#for items in items:
+ #   prices.append(items[1])
+
+prices = list(map(lambda item: item[1], items))
+prices = [item[1] for item in items] #use list comprehensions becuase they are cleaner and more performant 
+print(prices)
+
+
+filtered = list(filter(lambda item: item[1] >= 10, items))
+filtered = [item for item in items if item[1] >= 10]
+print(filtered)
+
+list1 = [1 ,2, 3]
+list2 = [10, 20, 30]
+
+print(list(zip("abc",list1, list2)))
+
+#LIFO - > last In - First Out stacks
+browsing_session = []
+browsing_session.append(1)
+browsing_session.append(2)
+browsing_session.append(3)
+print(browsing_session)
+last = browsing_session.pop()
+print(last)
+print(browsing_session)
+if not browsing_session:
+    print("redirect", browsing_session[-1])
+
+
+
