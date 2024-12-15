082e91c0b0d7869574801a26cdb57a7b8877a855
Joseph Kirkish <kirkish.2@gmail.com>
2024-12-14 20:32:56 -0500

    Added django project with moves and a Hello World Folder

---
+++ Hello_World/HelloWorld.py
@@ -0,0 +1,26 @@
+import requests
+
+response = requests.get("http://google.com")
+print(response)
+
+print("Hello World")
+
+
+# C:\Users\kirki\Coding_Projects\python_Practice>pipenv install
+# To activate this project's virtualenv, run pipenv shell.
+# Alternatively, run a command inside the virtualenv with pipenv run.
+
+# (kirki-baQyV_J0) C:\Users\kirki>cd Coding_Projects
+
+# (kirki-baQyV_J0) C:\Users\kirki\Coding_Projects>cd python_Practice
+
+# (kirki-baQyV_J0) C:\Users\kirki\Coding_Projects\python_Practice>cd Hello_World
+
+# (kirki-baQyV_J0) C:\Users\kirki\Coding_Projects\python_Practice\Hello_World>python HelloWorld.py
+# <Response [200]>
+# Hello World
+
+# (kirki-baQyV_J0) C:\Users\kirki\Coding_Projects\python_Practice\Hello_World>exit
+
+# C:\Users\kirki\Coding_Projects\python_Practice>
+
